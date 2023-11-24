9704093879ecbbcb4e235a79fcf54abeb92f660b
nakulsoneji <nakulsoneji@gmail.com>
2023-11-24 01:55:49 -0800

    added port identification

--- src/client.rs
+++ src/client.rs
@@ -27,6 +27,7 @@ impl Default for MyApp {
     fn default() -> Self {
         let sock = TcpStream::connect("127.0.0.1:8000").unwrap();
         sock.set_nonblocking(true).unwrap();
+        
         Self {
             text: String::from(""),
             socket: sock,
@@ -38,25 +39,20 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
+            ui.heading(format!("Client {}", self.socket.local_addr().unwrap().to_string()));
             let response = ui.add(egui::TextEdit::singleline(&mut self.text));
 
             if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                 println!("{}", self.text);
-                match self.socket.write(self.text.as_bytes()) {
-                    Ok(0) => {
-                        self.socket.shutdown(std::net::Shutdown::Write).expect("shutdown failed");
-                    }
+                match self.socket.write(format!("{}: {}", self.socket.local_addr().unwrap().port(), self.text).as_bytes()) {
                     Ok(_) => {}
                     Err(_) => {}
                 }
                 self.text = String::from("");
-                // …
             }
 
             let text: &mut [u8] = &mut [0; 127];
             match self.socket.read(text) {
-                
                 Ok(_) => {
                     self.messages.push(std::str::from_utf8(text).unwrap().to_owned());
                 }
@@ -64,7 +60,7 @@ impl eframe::App for MyApp {
             }
 
             for message in &self.messages {
-                ui.label(format!("message: {}", message));
+                ui.label(format!("{}", message));
             }
 
             ctx.request_repaint();
